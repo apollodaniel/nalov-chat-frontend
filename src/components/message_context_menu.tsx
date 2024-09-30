400512ed0b7c8f25ede1a409c427709b389bca7c
Apollo Daniel <developer.apollo.mail@gmail.com>
2024-09-30 14:57:45 -0300

    Fixed context menu appearance

--- src/components/message_context_menu.tsx
+++ src/components/message_context_menu.tsx
@@ -1,79 +1,79 @@
 import { Message, PositionOffset } from "../utils/types";
 
 interface IProps {
-	msg: Message;
-	chat_id: string;
-	onFocusExit: () => void;
-	onEdit: (msg: Message) => void;
-	onDelete: (msg: Message) => void;
-	onShowInfo: (msg: Message) => void;
-	position_offset?: PositionOffset;
+  msg: Message;
+  chat_id: string;
+  onFocusExit: () => void;
+  onEdit: (msg: Message) => void;
+  onDelete: (msg: Message) => void;
+  onShowInfo: (msg: Message) => void;
+  position_offset?: PositionOffset;
 }
 
 interface ContextMenuItemProps {
-	name: string;
-	event_callback: () => void;
+  name: string;
+  event_callback: () => void;
+  className?: string;
 }
 
-function ContextMenuItem({ name, event_callback }: ContextMenuItemProps) {
-	return (
-		<li
-			className="list-group-item list-group-item-action rounded-bottom-0"
-			style={{
-				cursor: "pointer"
-			}}
-			onClick={() => event_callback()}
-		>
-			{name}
-		</li>
-	);
+function ContextMenuItem({
+  name,
+  event_callback,
+  className,
+}: ContextMenuItemProps) {
+  return (
+    <li
+      className={`list-group-item list-group-item-action ${className || ""}`}
+      style={{
+        cursor: "pointer",
+      }}
+      onClick={() => event_callback()}
+    >
+      {name}
+    </li>
+  );
 }
 
 function MessageContextMenu({
-	msg,
-	chat_id,
-	onFocusExit,
-	onDelete,
-	onShowInfo,
-	onEdit,
-	position_offset
+  msg,
+  chat_id,
+  onFocusExit,
+  onDelete,
+  onShowInfo,
+  onEdit,
+  position_offset,
 }: IProps) {
-	return (
-		<ul
-			className="card  w-100 position-absolute list-group" // ${chat_id === msg.sender_id ? "align-self-start mx-3" : "align-self-end mx-3"}`}
-			onBlur={() => onFocusExit()}
-			onMouseDown={(event) => {
-				event.preventDefault();
-				event.stopPropagation();
-			}}
-			style={
-
-				{
-					maxWidth: "250px",
-					zIndex: "10",
-					top: `${position_offset?.offset_top || 0}px`,
-					left: `${position_offset?.offset_left|| 0}px`
-				}
-			}
-		>
-			{msg.sender_id != chat_id && (
-				<div>
-					<ContextMenuItem
-						name="Edit"
-						event_callback={() => onEdit(msg)}
-					/>
-					<ContextMenuItem
-						name="Delete"
-						event_callback={() => onDelete(msg)}
-					/>
-				</div>
-			)}
-			<ContextMenuItem
-				name="Show message information"
-				event_callback={() => onShowInfo(msg)}
-			/>
-		</ul>
-	);
+  return (
+    <ul
+      className="card  w-100 position-absolute rounded-3 list-group" // ${chat_id === msg.sender_id ? "align-self-start mx-3" : "align-self-end mx-3"}`}
+      onBlur={() => onFocusExit()}
+      onMouseDown={(event) => {
+        event.preventDefault();
+        event.stopPropagation();
+      }}
+      style={{
+        maxWidth: "250px",
+        zIndex: "10",
+        top: `${position_offset?.offset_top || 0}px`,
+        left: `${position_offset?.offset_left || 0}px`,
+      }}
+    >
+      {msg.sender_id != chat_id && (
+        <div>
+          <ContextMenuItem
+            name="Edit"
+            event_callback={() => onEdit(msg)}
+            className="rounded-top-3"
+          />
+          <ContextMenuItem name="Delete" event_callback={() => onDelete(msg)} />
+        </div>
+      )}
+      <ContextMenuItem
+        name="Show message information"
+        event_callback={() => onShowInfo(msg)}
+      />
+    </ul>
+  );
 }
 
 export default MessageContextMenu;
