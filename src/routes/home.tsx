b42581b06e3123cf71a07550cac23195a80cc6ed
Apollo Daniel <developer.apollo.mail@gmail.com>
2024-10-14 13:22:09 -0300

    Added dynamic search with 1500 second delay

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,97 +1,117 @@
-import { useEffect, useState } from "react";
-import { get_user_chats, listen_chats } from "../utils/functions/chat";
-import { ChatType, User } from "../utils/types";
-import { get_available_users } from "../utils/functions/user";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from 'react';
+import { get_user_chats, listen_chats } from '../utils/functions/chat';
+import { ChatType, User } from '../utils/types';
+import { get_available_users, get_user } from '../utils/functions/user';
+import { useNavigate } from 'react-router-dom';
 
-import HomeTreeDotsPopup from "../components/home_three_dots_popup";
-import ChatListItem from "../components/chat_list_item";
-import UserListItem from "../components/user_list_item";
-import "../css/home.css";
-import { toast_error_messages } from "../utils/constants";
+import HomeTreeDotsPopup from '../components/home_three_dots_popup';
+import ChatListItem from '../components/chat_list_item';
+import UserListItem from '../components/user_list_item';
+import '../css/home.css';
+import { toast_error_messages } from '../utils/constants';
 
 function Home() {
-  // main page
-  const [chats, setChats] = useState<ChatType[]>([]);
-  const [users, setUsers] = useState<User[]>([]);
-  const [search, setSearch] = useState<string>("");
-  const [focusedSearch, setFocusedSearch] = useState(false);
-  const [moreActionsPopupVisible, setMoreActionsPopupVisible] = useState(false);
+	// main page
+	const [chats, setChats] = useState<ChatType[]>([]);
+	const [users, setUsers] = useState<User[]>([]);
+	const [search, setSearch] = useState<string>('');
+	const [focusedSearch, setFocusedSearch] = useState(false);
+	const [moreActionsPopupVisible, setMoreActionsPopupVisible] =
+		useState(false);
 
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const get_users = async () => {
-    const users = await get_available_users();
-    setUsers(users);
-  };
+	const get_users = async () => {
+		const args =
+			search.trim().length === 0
+				? ''
+				: `?filter_field=username&filter_value=${search}`;
+		const users = await get_available_users(args);
+		setUsers(users);
+	};
 
-  const get_chats = async () => {
-    const chat_result = await get_user_chats();
-    setChats(chat_result);
+	const get_chats = async () => {
+		const chat_result = await get_user_chats();
+		setChats(chat_result);
 
-    listen_chats((chats: ChatType[]) => setChats(chats));
-  };
+		listen_chats((chats: ChatType[]) => setChats(chats));
+	};
 
-  useEffect(() => {
-    get_chats().then(() => {
-      get_users();
-    });
-  }, []);
+	useEffect(() => {
+		get_chats().then(() => {
+			get_users();
+		});
+	}, []);
 
-  return (
-    <div
-      id="home-container"
-      className="card d-flex flex-column my-5 gap-3 p-3"
-      onClick={(event) => {
-        if ((event.target as Element).id === "home-container")
-          setFocusedSearch(false);
-      }}
-    >
-      <div
-        className="d-flex flex-row gap-3 align-items-center justify-content-center"
-        style={{
-          height: "60px",
-        }}
-      >
-        <div className="form-floating w-100 h-100">
-          <input
-            type="search"
-            className="form-control h-100"
-            onFocus={() => setFocusedSearch(true)}
-            onChange={(event) => {
-              setSearch(event.target.value);
-            }}
-          />
-          <label>Search</label>
-        </div>
-        <button
-          onClick={() => setMoreActionsPopupVisible(true)}
-          id="more-actions-button"
-          className="btn btn-primary h-100"
-        >
-          More actions
-        </button>
-      </div>
-      <ul className="list-group border-0 rounded-3">
-        {!focusedSearch && search.length === 0
-          ? // chats
-            chats.map((c) => (
-              <ChatListItem key={c.user.id} navigate={navigate} chat={c} />
-            ))
-          : // users
-            users.map((u) => (
-              <UserListItem key={u.id} navigate={navigate} user={u} />
-            ))}
-      </ul>
-      {moreActionsPopupVisible && (
-        <HomeTreeDotsPopup
-          onCancel={() => setMoreActionsPopupVisible(false)}
-          show={moreActionsPopupVisible}
-          navigate={navigate}
-        />
-      )}
-    </div>
-  );
+	useEffect(() => {
+		setTimeout(() => {
+			get_users();
+		}, 1000);
+	}, [search]);
+
+	return (
+		<div
+			id="home-container"
+			className="card d-flex flex-column my-5 gap-3 p-3"
+			onClick={(event) => {
+				if ((event.target as Element).id === 'home-container')
+					setFocusedSearch(false);
+			}}
+		>
+			<div
+				className="d-flex flex-row gap-3 align-items-center justify-content-center"
+				style={{
+					height: '60px',
+				}}
+			>
+				<div className="form-floating w-100 h-100">
+					<input
+						type="search"
+						className="form-control h-100"
+						onFocus={() => setFocusedSearch(true)}
+						onChange={(event) => {
+							// setup timeout if it not exists
+							setSearch(event.target.value);
+						}}
+					/>
+					<label>Search</label>
+				</div>
+				<button
+					onClick={() => setMoreActionsPopupVisible(true)}
+					id="more-actions-button"
+					className="btn btn-primary h-100"
+				>
+					More actions
+				</button>
+			</div>
+			<ul className="list-group border-0 rounded-3">
+				{!focusedSearch
+					? // chats
+						chats.map((c) => (
+							<ChatListItem
+								key={c.user.id}
+								navigate={navigate}
+								chat={c}
+							/>
+						))
+					: // users
+						users.map((u) => (
+							<UserListItem
+								key={u.id}
+								navigate={navigate}
+								user={u}
+							/>
+						))}
+			</ul>
+			{moreActionsPopupVisible && (
+				<HomeTreeDotsPopup
+					onCancel={() => setMoreActionsPopupVisible(false)}
+					show={moreActionsPopupVisible}
+					navigate={navigate}
+				/>
+			)}
+		</div>
+	);
 }
 
 export default Home;
--- src/utils/functions/user.ts
+++ src/utils/functions/user.ts
@@ -1,145 +1,146 @@
 import { execRequest, get_current_host } from "./functions";
 import {
-  RefreshAuthTokenResult,
-  RegisterFormSubmit,
-  User,
-  UserCredentials,
+	RefreshAuthTokenResult,
+	RegisterFormSubmit,
+	User,
+	UserCredentials,
 } from "../types";
 import { toast_error_messages } from "../constants";
 
 export const register_user = (user: RegisterFormSubmit): Promise<void> =>
-  new Promise((r, _rj) =>
-    execRequest({
-      endpoint: `/auth/register`,
-      method: "POST",
-      customAuth: null,
-      options: { content: user, headers: { Authorization: null } },
-      errorMessage: toast_error_messages.register_error,
-      onSucess: r,
-      onFail: _rj,
-    }),
-  );
+	new Promise((r, _rj) =>
+		execRequest({
+			endpoint: `/auth/register`,
+			method: "POST",
+			customAuth: null,
+			options: { content: user, headers: { Authorization: null } },
+			errorMessage: toast_error_messages.register_error,
+			onSucess: r,
+			onFail: _rj,
+		}),
+	);
 
 export const login_user = (user: UserCredentials): Promise<void> =>
-  new Promise((r, _rj) =>
-    execRequest({
-      endpoint: `/auth/login`,
-      method: "POST",
-      options: { content: user },
-      customAuth: null,
-      errorMessage: toast_error_messages.login_error,
-      onSucess: r,
-      onFail: _rj,
-    }),
-  );
+	new Promise((r, _rj) =>
+		execRequest({
+			endpoint: `/auth/login`,
+			method: "POST",
+			options: { content: user },
+			customAuth: null,
+			errorMessage: toast_error_messages.login_error,
+			onSucess: r,
+			onFail: _rj,
+		}),
+	);
 
-export const get_available_users = (): Promise<User[]> =>
-  new Promise((r, _rj) =>
-    execRequest({
-      endpoint: `/api/users`,
-      method: "GET",
-      errorMessage: toast_error_messages.get_users_error,
-      onSucess: r,
-    }),
-  );
+export const get_available_users = (params?: string): Promise<User[]> =>
+	new Promise((r, _rj) =>
+		execRequest({
+			endpoint: `/api/users${params || ""}`,
+			method: "GET",
+			errorMessage: toast_error_messages.get_users_error,
+			onSucess: r,
+		}),
+	);
 
 export const get_user = (id: string): Promise<User> =>
-  new Promise((r, _rj) =>
-    execRequest({
-      endpoint: `/api/users/${id}`,
-      method: "GET",
-      onSucess: r,
-      errorMessage: toast_error_messages.get_users_error,
-    }),
-  );
+	new Promise((r, _rj) =>
+		execRequest({
+			endpoint: `/api/users/${id}`,
+			method: "GET",
+			onSucess: r,
+			errorMessage: toast_error_messages.get_users_error,
+		}),
+	);
 
 export const get_current_user = (): Promise<User> =>
-  new Promise((r, _rj) =>
-    execRequest({
-      endpoint: `/api/users/current`,
-      method: "GET",
-      errorMessage: toast_error_messages.get_users_error,
-      onSucess: r,
-    }),
-  );
+	new Promise((r, _rj) =>
+		execRequest({
+			endpoint: `/api/users/current`,
+			method: "GET",
+			errorMessage: toast_error_messages.get_users_error,
+			onSucess: r,
+		}),
+	);
 
 export const logout_user = (): Promise<User> =>
-  new Promise((r, _rj) =>
-    execRequest({
-      endpoint: `/auth/logout`,
-      method: "POST",
-      onSucess: () => {
-        window.localStorage.clear();
-        window.sessionStorage.clear();
-        window.open(window.location.href, "_self");
-      },
-    }),
-  );
+	new Promise((r, _rj) =>
+		execRequest({
+			endpoint: `/auth/logout`,
+			method: "POST",
+			onSucess: () => {
+				window.localStorage.clear();
+				window.sessionStorage.clear();
+				window.open(window.location.href, "_self");
+			},
+		}),
+	);
 
 // token related functions
 export async function refresh_user_token(): Promise<void> {
-  const refresh_token = window.localStorage.getItem("refresh_token");
+	const refresh_token = window.localStorage.getItem("refresh_token");
 
-  if (!refresh_token) {
-    window.sessionStorage.clear();
-    window.localStorage.clear();
-    throw new Error("unauthorized");
-  }
+	if (!refresh_token) {
+		window.sessionStorage.clear();
+		window.localStorage.clear();
+		throw new Error("unauthorized");
+	}
 
-  const result = await fetch(get_current_host("/auth/token"), {
-    headers: {
-      Authorization: `Bearer ${refresh_token!}`,
-    },
-  });
+	const result = await fetch(get_current_host("/auth/token"), {
+		headers: {
+			Authorization: `Bearer ${refresh_token!}`,
+		},
+	});
 
-  if (result.ok) {
-    const refresh_token_result: RefreshAuthTokenResult = await result.json();
-    window.sessionStorage.setItem(
-      "auth_token",
-      refresh_token_result.auth_token,
-    );
-  }
+	if (result.ok) {
+		const refresh_token_result: RefreshAuthTokenResult =
+			await result.json();
+		window.sessionStorage.setItem(
+			"auth_token",
+			refresh_token_result.auth_token,
+		);
+	}
 }
 
 export async function get_auth_token(): Promise<string> {
-  let auth_token = window.sessionStorage.getItem("auth_token");
-  if (!auth_token) await refresh_user_token();
+	let auth_token = window.sessionStorage.getItem("auth_token");
+	if (!auth_token) await refresh_user_token();
 
-  auth_token = window.sessionStorage.getItem("auth_token");
-  if (!auth_token) {
-    throw new Error("error while trying to get user auth token");
-  }
+	auth_token = window.sessionStorage.getItem("auth_token");
+	if (!auth_token) {
+		throw new Error("error while trying to get user auth token");
+	}
 
-  return auth_token;
+	return auth_token;
 }
 
 export const check_user_logged_in = async (onFail: () => void) => {
-  const token = await get_auth_token();
-  return await new Promise((r) =>
-    execRequest({
-      endpoint: "/auth/check-token",
-      method: "POST",
-      options: {
-        content: {
-          token: token,
-          type: "Auth",
-        },
-      },
-      onSucess: r,
-      onFail: () => onFail(),
-    }),
-  );
+	const token = await get_auth_token();
+	return await new Promise((r) =>
+		execRequest({
+			endpoint: "/auth/check-token",
+			method: "POST",
+			options: {
+				content: {
+					token: token,
+					type: "Auth",
+				},
+			},
+			onSucess: r,
+			onFail: () => onFail(),
+		}),
+	);
 };
 
 export const delete_user = (onFail: () => void) =>
-  execRequest({
-    endpoint: "/auth/account",
-    method: "DELETE",
-    errorMessage: toast_error_messages.delete_account_error,
-    onSucess: () => {
-      window.localStorage.clear();
-      window.sessionStorage.clear();
-      window.open(window.location.href, "_self");
-    },
-    onFail: onFail,
-  });
+	execRequest({
+		endpoint: "/auth/account",
+		method: "DELETE",
+		errorMessage: toast_error_messages.delete_account_error,
+		onSucess: () => {
+			window.localStorage.clear();
+			window.sessionStorage.clear();
+			window.open(window.location.href, "_self");
+		},
+		onFail: onFail,
+	});
