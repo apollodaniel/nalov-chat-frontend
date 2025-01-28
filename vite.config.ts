5f75563583e91cbf19c9d186e9838e7a04965dfd
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-01-27 23:31:18 -0300

    Removed unnecessary packages

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import { createRequire } from 'node:module';
 
 import { defineConfig } from 'vite';
 import { defineConfig, normalizePath } from 'vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-
-const require = createRequire(import.meta.url);
-
-const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
-const cMapsDir = normalizePath(path.join(pdfjsDistPath, 'cmaps'));
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -16,22 +10,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 
-	build: {
-		rollupOptions: {
-			external: ["@emotion/react"]
-		}
-	},
+//	build: {
+//		rollupOptions: {
+//			external: ["@emotion/react"]
+//		}
+//	},
 	plugins: [react(),
 
 
-	viteStaticCopy({
-		targets: [
-			{
-				src: cMapsDir,
-				dest: '',
-			},
-		],
-	}),
 	],
 })
 
